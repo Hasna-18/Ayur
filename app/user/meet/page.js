59830ef6ca8b4f1ad68c960089980c4eae223466
Hasna <hasnaabida18@gmail.com>
2025-11-18 23:25:30 +0530

    production ready

---
+++ app/user/meet/page.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function MeetPage() {
+  return (
+    <div className="p-6 text-white">
+      <h1 className="text-2xl font-bold">Meet</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+}
